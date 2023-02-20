397dd7cc97a4487e48265bbc50b9069f51446add
Morella92 <morena.piem@outlook.com>
2023-02-20 16:32:46 +0100

    START v-for in html

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+
+            activeSlide: 0,
+
+            slides: [
+                {
+                    image: './img/Barcellona.jpg',
+                    title: 'Capital of Spain',
+                    text: 'Park Guell',
+                }, {
+                    image: './img/Berlino.jpg',
+                    title: 'Capital of Germany',
+                    text: 'Porta di Brandeburgo',
+                }, {
+                    image: './img/Londra.jpg',
+                    title: 'Capital of England',
+                    text: 'Tower Bridge',
+                }, {
+                    image: './img/Parigi.jpg',
+                    title: 'Capital of France',
+                    text: 'Torre Eiffel',
+                }, {
+                    image: './img/Roma.jpg',
+                    title: 'Capital of Italy',
+                    text: 'Colosseo',
+                }
+            ]
+        }
+    },
+
+    methods: {
+
+        previousSLide() {
+
+            
+
+        }
+    }
+
+}).mount('#app')
